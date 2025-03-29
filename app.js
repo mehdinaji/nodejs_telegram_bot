1a80f1c7040ccd2596d5334b19e7891823b03d30
mehdinaji <seyedmehdinaji@gmail.com>
2025-03-30 03:11:37 +0330

    env

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
+require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = '7742267767:AAHcptlWkEL1ceBmvH5H4FfPkOf40k27-tA';
+const token = process.env.TELEGRAM_BOT_TOKEN;
+
+if (!token) {
+  console.error("❌ Error: TELEGRAM_BOT_TOKEN is missing in .env file!");
+  process.exit(1);
+}
 
 const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/time/, (msg) => {
   const chatId = msg.chat.id;
   const now = new Date();
-  const timeString = now.toLocaleTimeString("fa-IR"); 
+  const timeString = now.toLocaleTimeString("fa-IR");
 
-  bot.sendMessage(chatId, `⏰ زمان فعلی: ${timeString}`)
-  });
+  bot.sendMessage(chatId, `⏰ زمان فعلی: ${timeString}`);
+});
